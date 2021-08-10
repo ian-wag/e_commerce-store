f87a82a1f30200fc9a014413d3e21a4a9687432f
EN <iwagner76@gmail.com>
2021-08-10 15:02:04 -0400

    added product and product styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Products from "./components/Products/Products";
 
 const App = () => {
-  return <div>E-commerce</div>;
+  return (
+    <div>
+      <Products />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-import Grid from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
 
 const products = [
-  { id: 1, name: "Shoes", description: "Running Shoes." },
-  { id: 2, name: "Macbook", description: "Apple macbook." },
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Travis Scott x Fragment x Air Jordan 1",
+    price: "$170",
+    image:
+      "https://sneakerbardetroit.com/wp-content/uploads/2021/07/Travis-Scott-Fragment-Air-Jordan-1-Low-DM7866-140-Release-Date.jpg",
+  },
+  {
+    id: 2,
+    name: "Shoes",
+    description: "Nike Dunk Low Black Multi Camo",
+    price: "$110",
+    image:
+      "https://sneakerbardetroit.com/wp-content/uploads/2021/07/Nike-Dunk-Low-Black-Multi-Camo-DH0957-001-Release-Date.jpg",
+  },
 ];
 
 const Products = () => {
-  <main>
-    <Grid container justify="center" spacing={4}>
-      {products.map((product) => (
-        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-          <Product />
-        </Grid>
-      ))}
-    </Grid>
-  </main>;
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
 };
 
 export default Products;
