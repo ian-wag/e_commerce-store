f2f6e829702e3be7d9e8acb4f0d4cca5368a4024
EN <iwagner76@gmail.com>
2021-08-12 15:33:15 -0400

    added custom fields to checkout form

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -7,10 +7,32 @@ import {
   Grid,
   Typography,
 } from "@material-ui/core";
-import { useForm } from "react-hook-form";
+import { useForm, FormProvider } from "react-hook-form";
+import FormInput from "./Checkout/CustomTextField";
 
 const AddressForm = () => {
-  return <div></div>;
+  const methods = useForm();
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping Address
+      </Typography>
+      <FormProvider {...methods}>
+        <form onSubmit="">
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="First Name" />
+            <FormInput required name="lastName" label="Last Name" />
+            <FormInput required name="address1" label="Address" />
+            <FormInput required name="address2" label="Unit/Apartment" />
+            <FormInput required name="email" label="Email" />
+            <FormInput required name="city" label="City" />
+            <FormInput required name="state" label="State" />
+            <FormInput required name="zip" label="Zip code" />
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
 };
 
 export default AddressForm;
