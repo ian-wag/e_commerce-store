12079915a904f5da39a55320ce832039fac2d799
EN <iwagner76@gmail.com>
2021-08-12 18:24:23 -0400

    added state/region fetch

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -14,23 +14,48 @@ import FormInput from "./Checkout/CustomTextField";
 const AddressForm = ({ checkoutToken }) => {
   const [shippingCountries, setShippingCountries] = useState([]);
   const [shippingCountry, setShippingCountry] = useState("");
-  const [shippingStates, setshippingStates] = useState([]);
-  const [shippingState, setShippingState] = useState("");
+  const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+  const [shippingSubdivision, setShippingSubdivision] = useState("");
   const [shippingOptions, setShippingOptions] = useState([]);
   const [shippingOption, setShippingOption] = useState("");
   const methods = useForm();
+
+  const countries = Object.entries(shippingCountries).map(([code, name]) => ({
+    id: code,
+    label: name,
+  }));
+
+  const subdivisions = Object.entries(shippingSubdivisions).map(
+    ([code, name]) => ({
+      id: code,
+      label: name,
+    })
+  );
 
   const fetchShippingCountries = async (checkoutTokenId) => {
     const { countries } = await commerce.services.localeListShippingCountries(
       checkoutTokenId
     );
-    console.log(countries);
     setShippingCountries(countries);
+    setShippingCountry(Object.keys(countries)[0]);
+  };
+
+  const fetchSubdivisions = async (countryCode) => {
+    const { subdivisions } = await commerce.services.localeListSubdivisions(
+      countryCode
+    );
+
+    setShippingSubdivisions(subdivisions);
+    setShippingSubdivision(Object.keys(subdivisions)[0]);
   };
 
   useEffect(() => {
     fetchShippingCountries(checkoutToken.id);
   }, []);
+
+  useEffect(() => {
+    if (shippingCountry) fetchSubdivisions(shippingCountry);
+  }, [shippingCountry]);
 
   return (
     <>
@@ -46,25 +71,36 @@ const AddressForm = ({ checkoutToken }) => {
             <FormInput name="address2" label="Unit/Apartment" />
             <FormInput required name="email" label="Email" />
             <FormInput required name="city" label="City" />
-            <FormInput required name="state" label="State" />
-            <FormInput required name="zip" label="Zip code" />
-            {/* <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6}>
+              <InputLabel>State/Region</InputLabel>
+              <Select
+                value={shippingSubdivision}
+                fullWidth
+                onChange={(e) => setShippingSubdivision(e.target.value)}
+              >
+                {subdivisions.map((subdivision) => (
+                  <MenuItem key={subdivision.id} value={subdivision.id}>
+                    {subdivision.label}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <FormInput required name="zip" label="Zip code/Postal Code" />
+            <Grid item xs={12} sm={6}>
               <InputLabel>Country</InputLabel>
-              <Select value="" fullWidth onChange="">
-                <MenuItem key="" value="">
-                  Select Me
-                </MenuItem>
+              <Select
+                value={shippingCountry}
+                fullWidth
+                onChange={(e) => setShippingCountry(e.target.value)}
+              >
+                {countries.map((country) => (
+                  <MenuItem key={country.id} value={country.id}>
+                    {country.label}
+                  </MenuItem>
+                ))}
               </Select>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <InputLabel>State</InputLabel>
-              <Select value="" fullWidth onChange="">
-                <MenuItem key="" value="">
-                  Select Me
-                </MenuItem>
-              </Select>
-            </Grid>
-            <Grid item xs={12} sm={6}>
+            {/* <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Options</InputLabel>
               <Select value="" fullWidth onChange="">
                 <MenuItem key="" value="  ">
